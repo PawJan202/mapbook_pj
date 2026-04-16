afddb9b405d07bcf2649f6a747910a1158a7ab10
PawJan202 <pawel.jaworski01@student.wat.edu.pl>
2026-04-16 12:53:59 +0200

    wip: simple structure of users list

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ zmienna_3: str = 'Eliza'
 zmienna_4: str = 'Filip'
 zmienna_5: str = 'Ewelina'
 zmienna_6: str = 'Weronika'
-print(zmienna_1, zmienna_2)
-print(zmienna_3)
-print(zmienna_4)
-print(zmienna_5)
-print(zmienna_6)
+
+users: list = [zmienna_1, zmienna_2, zmienna_3, zmienna_4, zmienna_5, zmienna_6]
+
+for user in users:
+    print(user)
