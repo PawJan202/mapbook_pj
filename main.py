1ef74124533a7dca0e20243a45721429f40ee776
PawJan202 <pawel.jaworski01@student.wat.edu.pl>
2026-04-16 12:41:58 +0200

    wip: simple structure of users list

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print('Geodezja i geoinformatyka rzadzi!!!1111')
+zmienna_1: str = 'Oliwia'
+zmienna_2: str = 'Paweł'
+zmienna_3: str = 'Eliza'
+zmienna_4: str = 'Filip'
+zmienna_5: str = 'Ewelina'
+zmienna_6: str = 'Weronika'
+print(zmienna_1, zmienna_2)
+print(zmienna_3)
+print(zmienna_4)
+print(zmienna_5)
+print(zmienna_6)
